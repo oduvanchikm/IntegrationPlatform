4aff88b30a2dd5a3e91a1df46675aa918eb895c7
oduvanchikm <mkgubareva2005@gmail.com>
2026-03-23 00:31:35 +0400

    fix

--- IntegrationPlatform.TestConsumer/Program.cs
+++
@@ -1,7 +0,0 @@
-using IntegrationPlatform.TestConsumer;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
